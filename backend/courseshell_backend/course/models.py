99f1447cef24981b77145590059e5d246bc09d2e
ashmlk <arshiama@hotmail.com>
2021-05-09 18:18:12 -0400

    added course model

--- backend/courseshell_backend/course/models.py
+++ backend/courseshell_backend/course/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from base.models import BaseModel
+from django.core.validators import MaxLengthValidator
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+class Course(BaseModel):
+    code = models.CharField(max_length=50)
+    description = models.TextField(validators=[MaxLengthValidator(1000)])
+    university = models.ForeignKey("university.University", verbose_name=_("courses"), null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return '%s - %s' % (self.code, self.university)
+    
