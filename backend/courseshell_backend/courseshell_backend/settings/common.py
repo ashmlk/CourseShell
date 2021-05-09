cf1fc5b17c9d20b8d791517dbba487481a72008b
ashmlk <arshiama@hotmail.com>
2021-05-08 22:01:05 -0400

    updated master

--- backend/courseshell_backend/courseshell_backend/settings/common.py
+++ backend/courseshell_backend/courseshell_backend/settings/common.py
@@ -92,8 +92,7 @@ DATABASES = {
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST', default='localhost'),
-        'PORT':config('DB_PORT')
-        
+        'PORT':config('DB_PORT')  
     } 
 }
 
