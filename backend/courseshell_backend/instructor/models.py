2ff2c0f135634c34ede3c589d7694da2fbd9a120
ashmlk <arshiama@hotmail.com>
2021-05-09 20:33:16 -0400

    added instructor model

---
+++ backend/courseshell_backend/instructor/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from base.models import BaseModel
+from django.template.defaultfilters import slugify
+from django.utils.translation import ugettext_lazy as _
+
+class Instructor(BaseModel):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    slug = models.SlugField(_(""), max_length=254)
+    university = models.ManyToManyField("university.University", verbose_name=_("universities"))
+    courses = models.ManyToManyField("course.Course", verbose_name=_("courses"))
+    
+    def __str__(self):
+        return '%s %s' % (self.first_name, self.last_name)
+    
+    def save(self, *args, **kwargs):
+        self.first_name = self.first_name.strip().lower()
+        self.last_name = self.last_name.strip().lower()
+        if not self.slug:
+            self.slug = slugify('%s %s' % (self.first_name, self.last_name))
+        super(Instructor, self).save(*args, **kwargs)
+
