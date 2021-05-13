ed511496366c47756a12e8cd543901528020115c
ashmlk <arshiama@hotmail.com>
2021-05-12 20:44:53 -0400

    FIXED UUID4 ISSUE ON COURSE MODEL

--- backend/courseshell_backend/course/models.py
+++ backend/courseshell_backend/course/models.py
@@ -6,7 +6,7 @@ from django.utils.translation import ugettext_lazy as _
 
 class Course(BaseModel):
     
-    uuid = models.UUIDField(default=uuid.uuid4(), unique=True, editable=False)
+    uuid = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
     code = models.CharField(max_length=50)
     description = models.TextField(validators=[MaxLengthValidator(1000)])
     university = models.ForeignKey("university.University", verbose_name=_("courses"), null=True, on_delete=models.SET_NULL)
